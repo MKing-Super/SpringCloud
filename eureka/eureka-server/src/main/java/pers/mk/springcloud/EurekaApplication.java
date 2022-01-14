baa0a23cd9a1ecdcd20e5df66681154722a98f52
kun.ma <1216578277@qq.com>
2022-01-14 11:50:55 +0800

    eureka

--- eureka/eureka-server/src/main/java/pers/mk/springcloud/EurekaApplication.java
+++ eureka/eureka-server/src/main/java/pers/mk/springcloud/EurekaApplication.java
@@ -1,5 +1,7 @@
 package pers.mk.springcloud;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -7,9 +9,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EurekaApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class,args);
+        LOGGER.info(">>>>>>>>>>> Eureka Started~ >>>>>>>>>>>");
     }
 
 }
