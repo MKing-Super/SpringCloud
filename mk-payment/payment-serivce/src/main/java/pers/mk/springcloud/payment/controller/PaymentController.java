cd6772383c64285d2e220363d9a82268509cb15b
kun.ma <1216578277@qq.com>
2022-01-14 13:42:50 +0800

    service

--- mk-payment/payment-serivce/src/main/java/pers/mk/springcloud/payment/controller/PaymentController.java
+++ mk-payment/payment-serivce/src/main/java/pers/mk/springcloud/payment/controller/PaymentController.java
@@ -4,7 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pers.mk.springcloud.payment.common.CommonResult;
+import pers.mk.springcloud.payment.model.Payment;
+import pers.mk.springcloud.payment.service.PaymentService;
 
 import javax.annotation.Resource;
 import java.util.List;
--- mk-payment/payment-serivce/src/main/java/pers/mk/springcloud/payment/service/PaymentService.java
+++ mk-payment/payment-serivce/src/main/java/pers/mk/springcloud/payment/service/PaymentService.java
@@ -3,7 +3,9 @@ package pers.mk.springcloud.payment.service;
 import org.apache.ibatis.annotations.Param;
 import pers.mk.springcloud.payment.model.Payment;
 
+
 public interface PaymentService {
+
     int create(Payment payment);
 
     Payment getPaymentById(@Param("id") Long id);
