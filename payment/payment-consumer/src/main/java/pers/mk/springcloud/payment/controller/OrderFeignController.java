b148a7b7f1b5eb76ce7a42b837ce541ac3cab7ee
kun.ma <makun5201@163.com>
2022-02-28 15:43:20 +0800

    json

--- payment/payment-consumer/src/main/java/pers/mk/springcloud/payment/controller/OrderFeignController.java
+++ payment/payment-consumer/src/main/java/pers/mk/springcloud/payment/controller/OrderFeignController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import pers.mk.springcloud.payment.model.CommonResult;
 import pers.mk.springcloud.payment.model.Payment;
@@ -17,7 +18,7 @@ public class OrderFeignController {
     @Resource
     private PaymentFeignService paymentFeignService;
 
-    @GetMapping("/consumer/payment/get/{id}")
+    @RequestMapping(value = "/consumer/payment/get/{id}",method = {RequestMethod.GET})
     //占位符映射
     public CommonResult<Payment> getPaymentById(@PathVariable Long id){
         return paymentFeignService.getPaymentById(id);
